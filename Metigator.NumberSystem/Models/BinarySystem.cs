18170fd9b145311181ed919133637d7c22e8c46d
unknown <abdallahaliahmed517@gmail.com>
2023-08-19 13:27:54 +0300

    aaa

--- Metigator.NumberSystem/Models/BinarySystem.cs
+++ Metigator.NumberSystem/Models/BinarySystem.cs
@@ -4,6 +4,7 @@
     {
         public BinarySystem(string value)  // "123" is not , "101010101" is correct
         {
+            value.Guard("01", NumberBase.BINARY);
             this.Value = value; 
         }
     }
